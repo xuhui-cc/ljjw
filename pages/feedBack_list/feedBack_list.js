3d2c32959d79370410ba46b26aee9be5c1495e76
niuben <niuben.wudi@qq.com>
2020-06-04 17:57:01 +0800

    feebback_list 导航

--- pages/feedBack_list/feedBack_list.js
+++ pages/feedBack_list/feedBack_list.js
@@ -12,7 +12,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setUpNaviSize()
   },
 
   /**
@@ -62,5 +62,24 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  /**
+   * 设置自定义导航栏尺寸
+  */
+  setUpNaviSize: function () {
+    var menuButtonRect = wx.getMenuButtonBoundingClientRect()
+    var systemInfo = wx.getSystemInfoSync()
+    let naviBarHeight = menuButtonRect.bottom+10
+    let naviBarWidth = systemInfo.screenWidth
+    let saveBottom = systemInfo.screenHeight-systemInfo.safeArea.bottom
+    
+    this.setData ({
+      naviBarHeight: naviBarHeight,
+      naviBarWidth: naviBarWidth,
+      navibar_content_height: naviBarHeight - systemInfo.statusBarHeight,
+      statusBar_height: systemInfo.statusBarHeight,
+      saveBottom: saveBottom
+    })
+  },
 })
