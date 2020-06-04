adab751aee8a7ee8a5d7264a69fa509823c4f899
niuben <niuben.wudi@qq.com>
2020-06-04 20:22:33 +0800

    文件下载清除缓存功能

--- pages/my/my.js
+++ pages/my/my.js
@@ -2,6 +2,9 @@
 
 const app = getApp()
 Page({
+  
+  // 打开的文件路径 在onShow中删除文件
+  openFilePath: '',
 
   /**
    * 页面的初始数据
@@ -137,6 +140,7 @@ Page({
             console.log('返回自定义路径：')
             console.log(filePath)
 
+            that.openFilePath = filePath
             wx.openDocument({
               showMenu: true,
               filePath: filePath,
@@ -285,6 +289,8 @@ Page({
         app.updateWxLoginCode(res.code)
       },
     })
+
+    this.clearLocalFile()
     
   },
 
@@ -380,6 +386,31 @@ Page({
         }
       })
     }
+  },
+
+  /**
+   * 清除本地保存的文件
+  */
+  clearLocalFile: function() {
+    let that = this
+
+    if (this.openFilePath == '') {
+      return
+    }
+
+    let fs = wx.getFileSystemManager()
+    let filePath = this.openFilePath
+    fs.unlink({
+      filePath: filePath,
+      success (res) {
+        console.log("文件删除成功" + filePath)
+        that.openFilePath = ''
+      },
+      fail (res) {
+        console.log("文件删除失败"+filePath)
+        console.log(res)
+      }
+    })
   },
 
   /**
