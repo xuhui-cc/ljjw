3f32701b8fcd1de0ea53cd6337e516d1276158b5
niuben <niuben.wudi@qq.com>
2020-06-05 17:12:03 +0800

    反馈列表页面搭建

--- pages/feedBack_list/feedBack_list.js
+++ pages/feedBack_list/feedBack_list.js
@@ -7,6 +7,60 @@ Page({
   data: {
     // 顶部菜单选中下标
     menuSelectedIndex: 0,
+    // item列表
+    itemArray: [
+      {
+        title: "学习反馈",
+        type: 0
+      },
+      {
+        title: "生活反馈",
+        type: 1
+      },
+      {
+        title: "软件使用",
+        type: 2
+      },
+      {
+        title: "其他问题",
+        type: 3
+      }],
+
+    // 反馈列表
+    feedBackList:[
+      {
+        isread: 1, 
+        time: '2019.09.04 09:20', 
+        type: '软件使用', 
+        dealType:1, // 1-处理中 2-已处理 3-被驳回
+        content: "问题：我再使用小程序的时候，班级资料中的pdf格式文件打开有问题，如下图所示，打开后，是空白，或者是提示文件类型错误。问题：我再使用小程序的时候，班级资料中的pdf格式文件打开有问题，如下图所示，打开后，是空白，或者是提示文件类型错误。",
+        open: false,
+        pics: ['','']
+      },
+      {
+        isread: 0, 
+        time: '2019.09.04 09:20', 
+        type: '软件使用', 
+        dealType: 2,
+        content: "问题：我再使用小程序的时候，班级资料中的pdf格式文件打开有问题，打开后，是空白，或者是提示。",
+        open: false,
+        pics: ['',''],
+        dealTime: '2019.06.07 09:15',
+        dealContent: '这个问题已经修复，建议重新打开小程序即可解决。',
+        dealPics: ['', '', '', '']
+      },
+      {
+        isread: 0, 
+        time: '2019.09.04 09:20', 
+        type: '软件使用', 
+        dealType: 3,
+        content: "问题：我再使用小程序的时候，班级资料中的pdf格式文件打开有问题，打开后，是空白，或者是提示错误打开后，是空白，或者是提示错误或者是提示错误打开后。",
+        open: false,
+        pics: ['', '', '', ''],
+        dealTime: '2019.06.07 09:15',
+        dealContent: '这个问题已经修复，建议重新打开小程序即可解决。',
+        dealPics: ['', '']
+      }],
   },
 
   /**
@@ -84,18 +138,40 @@ Page({
       saveBottom: saveBottom
     })
   },
-  // ------------------------------------------------私有方法---------------------------------------------------
+  // ------------------------------------------------交互事件---------------------------------------------------
   /**
    * 顶部菜单 点击事件
   */
   menu_selected: function (e) {
-    console.log(e)
+    
     let index = e.currentTarget.dataset.index
     if (index == this.data.menuSelectedIndex) {
       return
     }
     this.setData({
       menuSelectedIndex: index
+    })
+  },
+
+  /**
+   * 导航栏返回按钮
+  */
+  back: function() {
+    wx.navigateBack({
+      complete: (res) => {},
+    })
+  },
+
+  /**
+   * 反馈列表 每个单元格 展开/收起 点击事件
+  */
+  feedBackOpenButton: function(e) {
+    // console.log(e)
+    let index = e.currentTarget.dataset.index
+    let feedBack = this.data.feedBackList[index]
+    let setData = "feedBackList["+index+"].open"
+    this.setData({
+      [setData]: !feedBack.open
     })
   }
 })
